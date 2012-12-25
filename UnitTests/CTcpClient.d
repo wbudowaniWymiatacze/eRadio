77d912bab7f818070fc08cbbafc3cebb0a2cc100
Artur Stepniak <aart.st@gmail.com>
2012-12-25 21:28:18 +0100

    more commit fixes

--- UnitTests/CTcpClient.d
+++
@@ -1,9 +0,0 @@
-objs/CTcpClient.o: ../TcpIp/src/CTcpClient.cpp \
- ../TcpIp/inc/CTcpClient.hpp ../Interface/globalDefs/gloDefs.hpp \
- ../Interface/globalDefs/types.hpp
-
-../TcpIp/inc/CTcpClient.hpp:
-
-../Interface/globalDefs/gloDefs.hpp:
-
-../Interface/globalDefs/types.hpp:
